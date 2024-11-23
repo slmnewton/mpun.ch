a3b8d15be70c0228a3fd65f10970590cd97ed714
Cole Wilkes <cole@ksub.com>
2024-11-23 00:46:33 -0500

    pump.fun link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-5xl text-white">
-      <a href="https://pump.fun/mpunch" target="_blank" rel="noopener noreferrer" className="cursor-pointer">$MPUNCH</a>
+      <a href="https://pump.fun/coin/4QHT9zvtzFofR3PUEJHGsG4B14CNVvzR9QizTtHVpump" target="_blank" rel="noopener noreferrer" className="cursor-pointer">$MPUNCH</a>
       <Image src={"/mpunch.svg"} alt="$MPUNCH" width={420} height={420}/>
     </div>
   )
